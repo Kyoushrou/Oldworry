7e78fbe77b75d72c4c910bd5384ab4bec5b16681
kyousiro.emi <kikigigi05120214@outlook.jp>
2020-12-11 12:28:31 +0900

    Initial Commit

---
+++ OldWorry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OldWorry
+//
+//  Created by 恵美　響詩郎 on 2020/12/11.
+//  Copyright © 2020 kyoushirou.emi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
