6f21971e4ecaeb50e145bacfd381dc7157d2b992
kyousiro.emi <kikigigi05120214@outlook.jp>
2020-12-13 11:59:29 +0900

    次はFirebaseの登録

--- OldWorry/ViewController.swift
+++ OldWorry/ViewController.swift
@@ -9,7 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var accountNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginBotton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func newAccountBotton(_ sender: Any) {
+        
+        if let name = accountNameTextField.text, let password = passwordTextField.text {
+            
+            if name.isEmpty || password.isEmpty {
+                
+                print("DEBUG_PRINT: 何かが空文字です。")
+                return
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
